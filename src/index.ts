52fe5eabb588a997017420650c109ca6d92e1900
async-devil <async.devil@gmail.com>
2021-07-08 00:37:29 +0300

    feat: added switch component to index file

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import Button from "./Button";
 import MessageBox from "./MessageBox";
 import ProgressBar from "./ProgressBar";
 import TrackBar from "./TrackBar";
+import Switch from "./Switch";
 
 import "./fonts/PixelUnicode/PixelUnicode.ttf";
 import "./index.css";
 
-export { Button, Alert, MessageBox, TrackBar, ProgressBar };
+export { Button, Alert, MessageBox, TrackBar, ProgressBar, Switch };
