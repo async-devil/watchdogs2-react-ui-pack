6a948234d22381c205c4d19f816401d09e690bf1
async-devil <async.devil@gmail.com>
2021-07-09 23:23:09 +0300

    lint: fixed order problem

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import Alert from "./Alert";
 import Button from "./Button";
 import MessageBox from "./MessageBox";
 import ProgressBar from "./ProgressBar";
-import TrackBar from "./TrackBar";
 import Switch from "./Switch";
+import TrackBar from "./TrackBar";
 
 import "./fonts/PixelUnicode/PixelUnicode.ttf";
 import "./index.css";
